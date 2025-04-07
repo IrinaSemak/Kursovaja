e9f25c7837855a05aa2b68f027e0a4c3a5c34bc0
IrinaSemak <irinasemak07@gmail.com>
2025-04-07 17:52:46 +0300

    Исправление концов строк

---
+++ pop4/ClusterInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace WildfireClustering
+{
+    public class ClusterInfo
+    {
+        public int ClusterId { get; set; }
+        public int PointCount { get; set; }
+        public Brush ClusterColor { get; set; }
+    }
+}
